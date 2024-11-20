a3c1ff8bae8281f574775891356f5747bd7bbdb5
olyasir <sirkinolya@gmail.com>
2024-11-20 15:10:21 +0200

    Update marian.sh

--- ci/models_build/marian.sh
+++ ci/models_build/marian.sh
@@ -1,7 +1,8 @@
 quantization="$1"
 language_pair="${2:-en-it}"
+device="${3:vulkan}"
 
 
 python -m mlc_llm convert_weight --model-type marian ./opus-mt-$language_pair/ --quantization "$quantization" --output "./output-$quantization-opus-mt-$language_pair"
 python -m mlc_llm gen_config --model-type marian ./opus-mt-$language_pair --quantization "$quantization" --conv-template marian --output "./output-$quantization-opus-mt-$language_pair"
-python -m mlc_llm compile --model-type marian "./output-$quantization-opus-mt-$language_pair/" --quantization "$quantization" --device vulkan --output "./output-$quantization-opus-mt-$language_pair/opus-mt-$language_pair.so"
+python -m mlc_llm compile --model-type marian "./output-$quantization-opus-mt-$language_pair/" --quantization "$quantization" --device "$device" --output "./output-$quantization-opus-mt-$language_pair/opus-mt-$language_pair.so"
