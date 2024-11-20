932ee1e3cd8167bfce9f5bc1ac19d8ea386173c7
olyasir <sirkinolya@gmail.com>
2024-11-20 12:22:45 +0200

    model compilation

--- ci/models_build/marian.sh
+++ ci/models_build/marian.sh
@@ -2,6 +2,6 @@ quantization="$1"
 language_pair="${2:-en-it}"
 
 
-mlc_llm convert_weight --model-type marian ./opus-mt-$language_pair/ --quantization "$quantization" --output "./output-$quantization-opus-mt-$language_pair"
-mlc_llm gen_config --model-type marian ./opus-mt-$language_pair --quantization "$quantization" --conv-template marian --output "./output-$quantization-opus-mt-$language_pair"
-mlc_llm compile --model-type marian "./output-$quantization-opus-mt-$language_pair/" --quantization "$quantization" --device vulkan --output "./output-$quantization-opus-mt-$language_pair/opus-mt-$language_pair.so"
+python -m mlc_llm convert_weight --model-type marian ./opus-mt-$language_pair/ --quantization "$quantization" --output "./output-$quantization-opus-mt-$language_pair"
+python -m mlc_llm gen_config --model-type marian ./opus-mt-$language_pair --quantization "$quantization" --conv-template marian --output "./output-$quantization-opus-mt-$language_pair"
+python -m mlc_llm compile --model-type marian "./output-$quantization-opus-mt-$language_pair/" --quantization "$quantization" --device vulkan --output "./output-$quantization-opus-mt-$language_pair/opus-mt-$language_pair.so"
