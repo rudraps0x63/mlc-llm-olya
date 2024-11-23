ad8fb5a9983f62d2fa0042f7952de71c03631a6b
olyasir <sirkinolya@gmail.com>
2024-11-23 07:15:54 +0000

    whisper

--- ci/models_build/whisper.sh
+++ ci/models_build/whisper.sh
@@ -3,5 +3,5 @@ device="${2:-vulkan}"
 
 
 python -m mlc_llm convert_weight --model-type whisper-tiny ./whisper-tiny/ --quantization "$quantization" --output "./output-whisper-$quantization"
-python -m mlc_llm gen_config --model-type whisper-tiny ./output-whisper-$language_pair --quantization "$quantization" --conv-template marian --output "./output-whisper-$quantization"
+python -m mlc_llm gen_config ./whisper-tiny --model-type whisper-tiny --quantization "$quantization" --conv-template whisper-tiny --output "./output-whisper-$quantization"
 python -m mlc_llm compile --model-type whisper-tiny "./output-whisper-$quantization/" --quantization "$quantization" --device "$device" --output "./output-whisper-$quantization.so"
