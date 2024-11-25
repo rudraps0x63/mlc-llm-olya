8470d60e591bcb18254d490d1dff86ed79468381
olyasir <sirkinolya@gmail.com>
2024-11-25 13:47:55 +0000

    fix bat

--- ci/models_build/marian.bat
+++ ci/models_build/marian.bat
@@ -1,8 +1,19 @@
-quantization="${1:q0f32}"
-language_pair="${2:-en-it}"
-device="${3:vulkan}"
+@echo off
+setlocal
 
+:: Default values
+set "quantization=q0f32"
+set "language_pair=en-it"
+set "device=vulkan"
 
-python -m mlc_llm convert_weight --model-type marian ./opus-mt-$language_pair/ --quantization "$quantization" --output "./output-$quantization-opus-mt-$language_pair"
-python -m mlc_llm gen_config --model-type marian ./opus-mt-$language_pair --quantization "$quantization" --conv-template marian --output "./output-$quantization-opus-mt-$language_pair"
-python -m mlc_llm compile --model-type marian "./output-$quantization-opus-mt-$language_pair/" --quantization "$quantization" --device "$device" --output "./output-$quantization-opus-mt-$language_pair/opus-mt-$language_pair.so"
+:: Check for provided arguments
+if not "%~1"=="" set "quantization=%~1"
+if not "%~2"=="" set "language_pair=%~2"
+if not "%~3"=="" set "device=%~3"
+
+:: Run the Python commands
+python -m mlc_llm convert_weight --model-type marian ./opus-mt-%language_pair%/ --quantization "%quantization%" --output "./output-%quantization%-opus-mt-%language_pair%"
+python -m mlc_llm gen_config --model-type marian ./opus-mt-%language_pair% --quantization "%quantization%" --conv-template marian --output "./output-%quantization%-opus-mt-%language_pair%"
+python -m mlc_llm compile --model-type marian "./output-%quantization%-opus-mt-%language_pair%/" --quantization "%quantization%" --device "%device%" --output "./output-%quantization%-opus-mt-%language_pair%/opus-mt-%language_pair%.so"
+
+endlocal
